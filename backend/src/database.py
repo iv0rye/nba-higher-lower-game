09d4dbb27253940cbd3773a48eca92f3aba64768
Ivan Li <ivanli10252006@gmail.com>
2026-06-11 22:41:20 +1000

    change function name of get db to get session

--- backend/src/database.py
+++ backend/src/database.py
@@ -13,6 +13,6 @@ engine = create_engine(DATABASE_URL)
 def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
-def get_db() -> Any:
+def get_session() -> Any:
     with Session(engine) as session:
         yield session
