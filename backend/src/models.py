f22c2b366b130daa87ccb81109630470906bbe96
Ivan Li <ivanli10252006@gmail.com>
2026-06-11 20:37:26 +1000

    Add player and playerseason models

---
+++ backend/src/models.py
@@ -0,0 +1,35 @@
+from sqlmodel import Field, Relationship, SQLModel
+
+class Player(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    nba_id: int = Field(unique=True)
+    name: str
+    positions: str | None = None
+    is_active: bool = True
+    photo_url: str | None = None
+
+    # linking relationships for easier queries
+    seasons: list["PlayerSeason"] = Relationship(back_populates="team")
+
+# 1 to many between Player, PlayerSeason
+class PlayerSeason(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    player_id: int = Field(foreign_key="player.id")
+    season: str
+    team: str | None = None
+
+    # testable statistics
+    games_played: int
+
+    points_per_game: float
+    assists_per_game: float
+    rebounds_per_game: float
+    steals_per_game: float
+    blocks_per_game: float
+    three_pm_per_game: float
+
+    field_goal_percentage: float | None = None
+    three_point_percentage: float | None = None
+    free_throw_percentage: float | None = None
+
+    player: Player | None = Relationship(back_populates="team")
