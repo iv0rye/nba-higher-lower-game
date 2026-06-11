dde5a727666dfcba4108786032b6325e279fa29d
Ivan Li <ivanli10252006@gmail.com>
2026-06-11 22:55:15 +1000

    test: return player data for home root to test data

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,31 @@
 from fastapi import FastAPI
-from .database import create_db_and_tables
+from sqlmodel import select
+
+from .dependencies import SessionDep
+from .database import create_db_and_tables, get_session, engine
 from . import models   # for initialising database
+from contextlib import asynccontextmanager
 
 # create dbs on start
-create_db_and_tables()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+    engine.dispose()
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root(session: SessionDep):
+    res = session.exec(
+        select(models.PlayerSeason, models.Player)
+        .join(models.Player, models.PlayerSeason.player_id == models.Player.id)
+        .where(models.PlayerSeason.season == '2010-11')
+    ).all() 
+
+    return [
+    {
+        "season": season,
+        "player": player,
+    } for season, player in res
+    ]
