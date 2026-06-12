3b62dd7f129ab331821c5f638f4a9f72f6bfa37c
Ivan Li <ivanli10252006@gmail.com>
2026-06-13 00:14:56 +1000

    add player get route linking to player_service

--- backend/src/routers/players.py
+++ backend/src/routers/players.py
@@ -1,6 +1,11 @@
 from fastapi import APIRouter
+from services import player_service
 
 router = APIRouter(
     prefix="/players",
     tags=["players"]
 )
+
+@router.get("/")
+async def get_players(q: str | None = None):
+    return player_service.get_players(q)
--- backend/src/services/player_service.py
+++ backend/src/services/player_service.py
@@ -6,9 +6,5 @@ from src.models import Player
 
 class PlayerService:
     @staticmethod
-    def get_players(session, sort_category: Any, limit: int = 50):
-        return session.exec(
-            select(Player)
-            .order_by(Player.sort_category.desc())
-            .limit(limit)
-        ).all()
+    def get_players(q):
+        pass
