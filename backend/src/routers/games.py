f1c41b207f729de9dacb7692434085657ffd8d83
Ivan Li <ivanli10252006@gmail.com>
2026-06-13 00:09:59 +1000

    fix: typo in fastapi routers with prefix

--- backend/src/routers/games.py
+++ backend/src/routers/games.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
 router = APIRouter(
-    prefix="\game",
-    tags=["game"],
+    prefix="/game",
+    tags=["game"]
 )
--- backend/src/routers/players.py
+++ backend/src/routers/players.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/players",
+    tags=["players"]
+)
