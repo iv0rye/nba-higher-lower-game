e4fb10cfc817726ef3cdf2a65d092f7171d6a718
Ivan Li <ivanli10252006@gmail.com>
2026-06-14 02:28:46 +1000

    wip: implement add game session into start game method

--- backend/src/routers/games.py
+++ backend/src/routers/games.py
@@ -1,10 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlmodel import SQLModel, Session
+from src.database import get_session
 
 router = APIRouter(
     prefix="/game",
     tags=["game"]
 )
 
+class StartGameRequest(SQLModel):
+    seasons: list[str] = []
+
 @router.post("/start/{category}/{type}")
-async def start_game(category: str, type: str):
+async def start_game(category: str, type: str, body: StartGameRequest, session: Session = Depends(get_session)):
     return
--- backend/src/services/game_service.py
+++ backend/src/services/game_service.py
@@ -1,4 +1,26 @@
+from sqlmodel import select
+from src.models import GameSession, Season
+from src.routers.games import StartGameRequest
+
 
 class GameService:
-    def __init__(self):
-        pass
+    @staticmethod
+    def start_game(cat: str, stat_type: str, body: StartGameRequest, session):
+        game_seasons: list[Season] = session.exec(
+            select(Season)
+            .where(Season.label.in_(body.seasons))
+        ).all()
+
+        new_game_session = GameSession(
+            score=0,
+            is_active=True,
+            stat_category=cat,
+            stat_type=stat_type,
+            seasons=game_seasons
+        )
+
+        session.add(new_game_session)
+        session.commit()
+
+        session.refresh(new_game_session)
+        return
