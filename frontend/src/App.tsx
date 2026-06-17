1f0ce45aacead786f6491274e33616193c3e54a9
Ivan Li <ivanli10252006@gmail.com>
2026-06-17 22:44:47 +1000

    add scene folder and remove old css code from template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,8 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
-import * as THREE from 'three'
+import HomePage from './views/HomePage'
 
-function SpinningBox() {
-  const meshRef = useRef<THREE.Mesh>(null)
-
-  useFrame((_, delta) => {
-    if (!meshRef.current) return
-    meshRef.current.rotation.x += delta
-    meshRef.current.rotation.y += delta
-  })
-
-  return (
-    <mesh ref={meshRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  )
-}
 
 export default function App() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <SpinningBox />
-      </Canvas>
-    </div>
+    <HomePage />
   )
 }
