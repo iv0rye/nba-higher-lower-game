936ea97d188561c78f41a8b165ede924499b2163
Ivan Li <ivanli10252006@gmail.com>
2026-06-19 21:58:16 +1000

    add new court texture, add tone mapping exposure to GL

--- frontend/src/scene/CourtScene.tsx
+++ frontend/src/scene/CourtScene.tsx
@@ -1,21 +1,20 @@
 import { Canvas } from '@react-three/fiber'
-import { useGLTF, OrbitControls, useTexture, Stars, Environment } from '@react-three/drei'
+import { useGLTF, OrbitControls, useTexture, Stars } from '@react-three/drei'
 import { useEffect } from 'react'
 import * as THREE from 'three'
-import { Bloom, EffectComposer, Scanline, Vignette } from '@react-three/postprocessing'
 
 function Court() {
   const { scene } = useGLTF('/models/basketball_court.glb')
   const texture = useTexture('/models/basketball_court.png')
 
   useEffect(() => {
-    texture.flipY = false
+    texture.flipY = true
     scene.traverse((child) => {
       if ((child as THREE.Mesh).isMesh) {
         const mesh = child as THREE.Mesh
         // map texture of court onto court flour (plane001)
         if (mesh.name === 'Plane001') {
-          mesh.material = new THREE.MeshStandardMaterial({ map: texture })
+          mesh.material = new THREE.MeshLambertMaterial({ map: texture })
         }
         // remove model baseplate (plane002)
         else if (mesh.name === 'Plane002') {
@@ -33,7 +32,7 @@ export default function BackgroundScene() {
     <Canvas
       camera={{ position: [0, 8, 14], fov: 55 }}
       style={{ position: 'absolute', top: 0, left: 0 }}
-      gl={{ antialias: true }}
+      gl={{ antialias: true, toneMappingExposure: 0.3 }}
     >
       {/* space environment */}
       <color attach="background" args={['#000008']} />
@@ -47,14 +46,9 @@ export default function BackgroundScene() {
       />
 
       {/* ambient lighting */}
-      <ambientLight intensity={0.1} color="#aaaaaa" />
-      <Environment preset="night" />
+      <ambientLight intensity={0.3} />
 
       <Court />
-
-      <EffectComposer>
-        <Vignette darkness={0.7} /> 
-      </EffectComposer>
 
       <OrbitControls />
     </Canvas>
