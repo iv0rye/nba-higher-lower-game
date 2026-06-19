f1ba69e28f0c99c7fa2c561e3205ae314b0500d6
Ivan Li <ivanli10252006@gmail.com>
2026-06-19 22:20:24 +1000

    add player props, basic player function returning primitive w player scene

--- frontend/src/scene/Player.tsx
+++ frontend/src/scene/Player.tsx
@@ -1,3 +1,5 @@
+import { useGLTF } from "@react-three/drei"
+
 const ANIMATIONS = [
   'CharacterArmature|Idle',
   'CharacterArmature|Run',
@@ -7,3 +9,23 @@ const ANIMATIONS = [
 ] as const
 
 type AnimationName = typeof ANIMATIONS[number]
+
+interface PlayerProps {
+  animation?: AnimationName
+  position?: [number, number, number]
+  rotation?: [number, number, number]
+}
+
+export default function Player(
+{ 
+  animation = 'CharacterArmature|Idle',
+  position = [0, 0, 0],
+  rotation = [0, 0, 0]
+} : PlayerProps) {
+
+  const { scene } = useGLTF('/models/character.glb')
+
+  return(
+    <primitive object={scene} />
+  )
+}
