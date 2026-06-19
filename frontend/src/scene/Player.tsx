61b2c260826b45fd489ca33ec1407c3bb66ea1a3
Ivan Li <ivanli10252006@gmail.com>
2026-06-19 22:14:55 +1000

    add animations type into player.tsx

---
+++ frontend/src/scene/Player.tsx
@@ -0,0 +1,9 @@
+const ANIMATIONS = [
+  'CharacterArmature|Idle',
+  'CharacterArmature|Run',
+  'CharacterArmature|Victory',
+  'CharacterArmature|Death',
+  'CharacterArmature|Defeat',
+] as const
+
+type AnimationName = typeof ANIMATIONS[number]
