0576b03ff53e5772e7699563cdb6ad1db1f99461
Ivan Li <ivanli10252006@gmail.com>
2026-06-20 22:45:56 +1000

    adjust camera hook by locking y axis movement and making camera overall more static. add rotation and influence constants for more static and less movement

--- frontend/src/hooks/useCameraMovement.ts
+++ frontend/src/hooks/useCameraMovement.ts
@@ -2,29 +2,34 @@ import { useFrame, useThree } from '@react-three/fiber'
 import { type RefObject } from 'react'
 import * as THREE from 'three'
 
-const OFFSET = new THREE.Vector3(0, 8, 14)
+const OFFSET = new THREE.Vector3(0, 12, 12) // camera offset is effectively distance from origin (player)
 const LERP = 0.05
 const CLAMP = { x: 8, z: 8 }
 
+const CAMERA_ROTATION = new THREE.Euler(-0.9, 0, 0)
+const CAMERA_INFLUENCE = 0.2
+
 interface Props {
-    playerRef: RefObject<THREE.Group | null>
+  playerRef: RefObject<THREE.Group | null>
 }
 
 export function useCameraMovement({ playerRef }: Props) {
   const { camera } = useThree()
 
+  camera.rotation.copy(CAMERA_ROTATION)
+	camera.position.y = OFFSET.y
+
   useFrame(() => {
-    if (!playerRef.current) return
+    if (!playerRef.current) 
+      return
 
     const pos = playerRef.current.position
 
-    const tx = THREE.MathUtils.clamp(pos.x, -CLAMP.x, CLAMP.x)
-    const tz = THREE.MathUtils.clamp(pos.z, -CLAMP.z, CLAMP.z)
+		// define positions to lerp into
+    const tx = THREE.MathUtils.clamp(pos.x * CAMERA_INFLUENCE, -CLAMP.x, CLAMP.x)
+    const tz = THREE.MathUtils.clamp(pos.z * CAMERA_INFLUENCE, -CLAMP.z, CLAMP.z)
 
     camera.position.x = THREE.MathUtils.lerp(camera.position.x, tx + OFFSET.x, LERP)
-    camera.position.y = THREE.MathUtils.lerp(camera.position.y, OFFSET.y, LERP)
     camera.position.z = THREE.MathUtils.lerp(camera.position.z, tz + OFFSET.z, LERP)
-
-    camera.lookAt(pos.x, 0, pos.z)
   })
 }
