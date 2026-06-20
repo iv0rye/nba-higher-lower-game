684fbec33506925f0e61a4b6123a2812f7f30f08
Ivan Li <ivanli10252006@gmail.com>
2026-06-20 20:04:55 +1000

    move keystate into types, add keysref into types for param type in other hooks

--- frontend/src/hooks/useKeys.ts
+++ frontend/src/hooks/useKeys.ts
@@ -1,14 +1,7 @@
 import { useEffect, useRef } from "react"
-import { GAME_KEYS, type GameKey } from "../types/game"
+import { GAME_KEYS, type GameKey, type KeyState } from "../types/game"
 import { useFrame } from "@react-three/fiber"
 import { FRAME_PRIORITIES } from "../config/framePriorities"
-
-interface KeyState {
-  pressed: boolean      // key is pressed in current frame
-  justPressed: boolean  // true for one frame after key press
-  justReleased: boolean // true for one frame after key release
-  holdDuration: number  // ms duration key has been held
-}
 
 // NOTE: to add a new key to be used in the game, please add it in the game keys type
 const GAME_KEY_SET = new Set<string>(GAME_KEYS)
--- frontend/src/types/game.ts
+++ frontend/src/types/game.ts
@@ -1,6 +1,17 @@
+import type { RefObject } from "react"
+
 export const GAME_KEYS = [
   'w', 'a', 's', 'd',
   'arrowup', 'arrowdown', 'arrowleft', 'arrowright'
 ] as const
 
 export type GameKey = typeof GAME_KEYS[number]
+
+export interface KeyState {
+  pressed: boolean      // key is pressed in current frame
+  justPressed: boolean  // true for one frame after key press
+  justReleased: boolean // true for one frame after key release
+  holdDuration: number  // ms duration key has been held
+}
+
+export type KeysRef = RefObject<Partial<Record<GameKey, KeyState>>>
