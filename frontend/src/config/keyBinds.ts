4c68a30f156d90000e37711685b1cfc733b49b9b
Ivan Li <ivanli10252006@gmail.com>
2026-06-20 20:30:16 +1000

    fix: change string type to gamekey type in keybinds interface

--- frontend/src/config/keyBinds.ts
+++ frontend/src/config/keyBinds.ts
@@ -2,17 +2,17 @@ import type { GameKey } from "../types/game"
 
 // Adding keybinds: Add name to keybinds interface then default key into default_bindings
 export interface keyBinds {
-  forward: string[]
-  backward: string[]
-  left: string[]
-  right: string[]
+  forward: GameKey[]
+  backward: GameKey[]
+  left: GameKey[]
+  right: GameKey[]
 }
 
 export const DEFAULT_BINDINGS: keyBinds = {
-  forward: ['w', 'arrowup'],
-  backward: ['s', 'arrowdown'],
-  left: ['a', 'arrowleft'],
-  right: ['d', 'arrowright']
+  forward: ['w', 'arrowup'] as GameKey[],
+  backward: ['s', 'arrowdown'] as GameKey[],
+  left: ['a', 'arrowleft'] as GameKey[],
+  right: ['d', 'arrowright'] as GameKey[]
 }
 
 export function isActionActive(
