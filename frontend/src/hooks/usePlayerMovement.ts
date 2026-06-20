0bac00854eac30e8dcb8aa5e35535a6e2c979660
Ivan Li <ivanli10252006@gmail.com>
2026-06-20 20:23:02 +1000

    add speed and clamp constatns and useframe hook

--- frontend/src/hooks/usePlayerMovement.ts
+++ frontend/src/hooks/usePlayerMovement.ts
@@ -1,9 +1,24 @@
-import { useEffect, useRef, type RefObject } from "react";
+import { type RefObject } from "react";
 import * as THREE from 'three'
 import type { KeysRef } from "../types/game";
+import { useFrame } from "@react-three/fiber";
+import { DEFAULT_BINDINGS } from "../config/keyBinds";
+import { vec2 } from "three/tsl";
+
+const SPEED = 5
+const LEVEL_AREA = [
+  [-8, 8], 
+  [-6, 6]
+]
 
 export function usePlayerMovement(
   playerRef: RefObject<THREE.Group>,
   keysRef: KeysRef
 ) {
+  useFrame((_, delta) => {
+    if (!playerRef.current || !keysRef.current) return
+
+    let moveX = 0
+    let moveZ = 0
+  })
 }
