08513eee362ede1fd209868f47a75c4c90942b27
Ivan Li <ivanli10252006@gmail.com>
2026-06-22 16:51:05 +1000

    add player card widget files with temporary content

---
+++ frontend/src/components/ui/PlayerCardWidget.tsx
@@ -0,0 +1,9 @@
+import styles from "./PlayerCardWidget.module.css"
+
+export default function PlayerCardWidget() {
+  return(
+    <div className={styles.panel}>
+      <span>player card</span>
+    </div>
+  )
+}
--- frontend/src/views/UIOverlayView.tsx
+++ frontend/src/views/UIOverlayView.tsx
@@ -1,3 +1,4 @@
+import PlayerCardWidget from "../components/ui/PlayerCardWidget";
 import ScoreWidget from "../components/ui/ScoreWidget";
 import styles from "./UIOverlayView.module.css"
 
@@ -5,6 +6,7 @@ export default function UIOverlayView() {
   return (
     <div className={styles.overlay}>
       <div className={styles.top}>
+        <PlayerCardWidget />
         <ScoreWidget score={0}/>
       </div>
     </div>
