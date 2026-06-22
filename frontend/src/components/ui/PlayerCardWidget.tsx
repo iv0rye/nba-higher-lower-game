89cdb0ed52d7e73f4c83e9f943acda6e852e786e
Ivan Li <ivanli10252006@gmail.com>
2026-06-22 17:00:44 +1000

    add headshot card to player card

--- frontend/src/components/ui/PlayerCardWidget.tsx
+++ frontend/src/components/ui/PlayerCardWidget.tsx
@@ -3,7 +3,7 @@ import styles from "./PlayerCardWidget.module.css"
 export default function PlayerCardWidget() {
   return(
     <div className={styles.panel}>
-      <span>player card</span>
+      <img src="https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png" alt="name" className={styles.headshot} />
     </div>
   )
 }
