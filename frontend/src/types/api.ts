7c120cffa773eeb4ee7436a9e852fa4e413cb3e5
Ivan Li <ivanli10252006@gmail.com>
2026-06-24 16:29:32 +1000

    add types for req and res schemas to backend

---
+++ frontend/src/types/api.ts
@@ -0,0 +1,29 @@
+export interface PlayerPreview {
+  id: number
+  name: string
+  photo_url: string | null
+  team: string | null
+  season: string | null
+  stat_category: string
+}
+
+export interface PlayerStat extends PlayerPreview {
+  stat_value: number
+}
+
+export interface NewGameResponse {
+  session_token: string
+  game_id: number
+  stat_category: string
+  stat_type: string
+  player_a: PlayerStat
+  player_b: PlayerPreview 
+}
+
+export interface GuessResponse {
+  is_correct: boolean
+  score: number
+  session_active: boolean
+  player_b: PlayerStat  
+  next_round: NewGameResponse | null
+}
