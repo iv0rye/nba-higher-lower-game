b186324691be9579ea6a0c9ae167f30f9e63a07e
Ivan Li <ivanli10252006@gmail.com>
2026-06-26 21:05:19 +1000

    add props into game engine hook

--- frontend/src/hooks/useGameEngine.ts
+++ frontend/src/hooks/useGameEngine.ts
@@ -1,3 +1,8 @@
-export function useGameEngine() {
-	
+interface GameEngineProps {
+	stat_type: string,
+	stat_category: string
+}
+
+export function useGameEngine({ stat_type, stat_category }: GameEngineProps) {
+
 }
