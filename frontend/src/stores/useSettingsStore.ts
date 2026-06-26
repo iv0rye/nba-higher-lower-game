aac956a9f35f906aba410546d4ac6206bf7375fe
Ivan Li <ivanli10252006@gmail.com>
2026-06-26 21:37:03 +1000

    implement settings store function

--- frontend/src/stores/useSettingsStore.ts
+++ frontend/src/stores/useSettingsStore.ts
@@ -1,7 +1,30 @@
 import { create } from "zustand";
+import type { StartGameRequest } from "../types/api";
 
 interface SettingsStore {
+	statType: string
+	statCategory: string
+	seasons: string[]
 
+	setStatType: (statType: string) => void
+	setStatCategory: (statCategory: string) => void
+	setSeasons: (seasons: string[]) => void 
+
+	getGameRequest: () => StartGameRequest
 }
 
-export const useSettingsStore = create<SettingsStore>()
+export const useSettingsStore = create<SettingsStore>((set, get) => ({
+	statType: 'career',
+	statCategory: 'ppg',
+	seasons: [],
+
+	setStatType: (statType) => set({ statType }),
+	setStatCategory: (statCategory) => set({ statCategory }),
+	setSeasons: (seasons) => set({ seasons }),
+
+	getGameRequest: () => ({
+    type: get().statType,
+    category: get().statCategory,
+    seasons: get().seasons,
+  })
+}))
