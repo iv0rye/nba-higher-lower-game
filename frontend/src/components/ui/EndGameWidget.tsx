6d557db982a68b7e4a300fd46d9056017c92bd5b
Ivan Li <ivanli10252006@gmail.com>
2026-07-01 18:50:54 +1000

    feat: end game widget to display after game has ended

--- frontend/src/components/ui/EndGameWidget.tsx
+++ frontend/src/components/ui/EndGameWidget.tsx
@@ -1,7 +1,64 @@
 import { useGameHistory } from "../../hooks/useGameHistory";
 import { useGameStore } from "../../stores/useGameStore";
+import MenuButtonWidget from "./MenuButtonWidget";
+import styles from "./EndGameWidget.module.css"
+import PlayerCardWidget from "./PlayerCardWidget";
+import type { GetGameRoundResponse } from "../../types/api";
+
+function getGuessLabel(round: GetGameRoundResponse): string {
+  if (round.guess_a_higher_b === null) return 'none'
+  return round.guess_a_higher_b ? 'higher' : 'lower'
+}
 
 export default function EndGameWidget() {
   const { score, sessionToken } = useGameStore.getState()
+  const reset = useGameStore((state) => state.reset)
   const { history, loading } = useGameHistory(sessionToken)
+
+  return (
+    <div className={styles.panel}>
+      <span className={styles.title}>Game over... Your score was:</span>
+      <span className={styles.score}>{score}</span>
+
+      {loading && <span className={styles.loading}>Loading...</span>}
+
+      <div className={styles.rounds}>
+
+        {/* creates a scrollbox with each round in the game just played */}
+        {history?.rounds.map((round, i) => (
+          <div key={round.id} className={styles.round}>
+
+            <span className={styles.roundNumber}>Round {i + 1}</span>
+
+            <span>
+              You guessed <b>{round.player_a.name}</b> had a 
+              <b> {getGuessLabel(round)} {round.player_a.stat_category.toUpperCase()} </b>
+              than <b>{round.player_b.name}</b> 
+            </span>
+
+            <div className={styles.players}>
+
+              <PlayerCardWidget
+                name={round.player_a.name}
+                headshotURL={round.player_a.photo_url}
+                statLabel={round.player_a.stat_category}
+                stat={round.player_a.stat_value}
+              />
+
+              <PlayerCardWidget
+                name={round.player_b.name}
+                headshotURL={round.player_b.photo_url}
+                statLabel={round.player_b.stat_category}
+                stat={round.player_b.stat_value}
+              />
+
+            </div>
+
+          </div>
+        ))}
+      </div>
+
+      <MenuButtonWidget text="Play Again" clickEvent={reset} />
+    </div>
+  )
 }
