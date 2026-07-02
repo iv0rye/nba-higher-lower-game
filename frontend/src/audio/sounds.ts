cbc6fb60d331c3b2effa52688b2325a289d9c05d
Ivan Li <ivanli10252006@gmail.com>
2026-07-02 16:35:55 +1000

    refactor audio folder into sfx and bgm, add bgm files

--- frontend/src/audio/sounds.ts
+++ frontend/src/audio/sounds.ts
@@ -1,12 +1,23 @@
 import { Howl } from 'howler'
 
 const sounds = {
-  select:       new Howl({ src: ['/audio/sci_fi_select.wav'], volume: 0.5 }),
-  hover:        new Howl({ src: ['/audio/sci_fi_hover.wav'], volume: 0.3 }),
-  footstep:     new Howl({ src: ['/audio/footstep.wav'], volume: 0.07 }),
-  lose:         new Howl({ src: ['/audio/lose_round.wav'], volume: 0.6 }),
-  tick:         new Howl({ src: ['/audio/clock_tick.wav'], volume: 0.4 }),
-  win:          new Howl({ src: ['/audio/win_round.wav'], volume: 0.6 }),
+  select:       new Howl({ src: ['/audio/sfx/sci_fi_select.wav'], volume: 0.5 }),
+  hover:        new Howl({ src: ['/audio/sfx/sci_fi_hover.wav'], volume: 0.3 }),
+  footstep:     new Howl({ src: ['/audio/sfx/footstep.wav'], volume: 0.07 }),
+  lose:         new Howl({ src: ['/audio/sfx/lose_round.wav'], volume: 0.6 }),
+  tick:         new Howl({ src: ['/audio/sfx/clock_tick.wav'], volume: 0.4 }),
+  win:          new Howl({ src: ['/audio/sfx/win_round.wav'], volume: 0.6 }),
+
+  menu_bgm: new Howl({ 
+    src: ['/audio/bgm/8-bit-life-menu-bgm.wav'], 
+    volume: 0.3, 
+    loop: true
+	}),
+	in_game_bgm: new Howl({ 
+    src: ['/audio/bgm/8-bit-rydeen-in-game-bgm.wav'], 
+    volume: 0.3, 
+    loop: true
+	})
 }
 
 export default sounds
